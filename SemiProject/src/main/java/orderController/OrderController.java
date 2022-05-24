352d4a3e02ddd4927d11b57555c298566beca8eb
cszzang89@gamail.com <cszzang89@gmail.com>
2022-05-24 14:18:54 +0900

    test3

--- SemiProject/src/main/java/orderController/OrderController.java
+++ SemiProject/src/main/java/orderController/OrderController.java
@@ -11,7 +11,7 @@ import order.orderDAO.OrderDAO;
 import order.orderDTO.OrderDTO;
 
 
-@WebServlet("/*.asd")
+@WebServlet("*.order")
 public class OrderController extends HttpServlet{
 	
 	
@@ -23,46 +23,45 @@ public class OrderController extends HttpServlet{
 	    OrderDTO dto = new OrderDTO();
 	    request.setCharacterEncoding("utf-8");
 	    
-	    if(uri.equals("/form.order")) {
-	    	response.sendRedirect("/order/order.jsp");
-	    	 String memberId =   request.getParameter("memberId");  			//2.회원아이디 
-		     String orderName =  request.getParameter("orderName");  		//3.수취인 이름 
-		     String orderContact =  request.getParameter("orderContect");	//4.수취인 연락처 
-		     String orderAddress =  request.getParameter("orderAddress");  	//5.수취인 주소 
-		     String orderMsg =  request.getParameter("orderMsg");  			//6.주문 메시지 
-		     String postMsg =  request.getParameter("postMsg");  			//7.배송 메시지 
-		     String payNumber =  request.getParameter("payNumber");  		//8.결제 번호  
-		     String productCode =  request.getParameter("productCode");  	//9.상품코드  
-		     String totalPay =  request.getParameter("totalPay");  			//10.결제금액 
-		     String card =  request.getParameter("card");  					//11.카드정보  
-		     
-		     dto.setMemberId(memberId);
-		     dto.setOrderName(orderName);
-		     dto.setOrderContact(orderContact);
-		     dto.setOrderAddress(orderAddress);
-		     dto.setOrderMsg(orderMsg);
-		     dto.setPostMsg(postMsg);
-		     dto.setPayNumber(payNumber);
-		     dto.setProductCode(productCode);
-		     dto.setTotalPay(totalPay);
-		     dto.setCard(card);
-		     OrderDAO dao = new OrderDAO();
-		     
-		     int inputResult =dao.insert(dto);
+	    	if(uri.equals("/form.order")) {
+	    		response.sendRedirect("/order/order.jsp");	
+	    	 	 
+	    		String memberId =   request.getParameter("memberId");  			//2.회원아이디 
+			     String orderName =  request.getParameter("orderName");  		//3.수취인 이름 
+			     String orderContact =  request.getParameter("orderContect");	//4.수취인 연락처 
+			     String orderAddress =  request.getParameter("orderAddress");  	//5.수취인 주소 
+			     String orderMsg =  request.getParameter("orderMsg");  			//6.주문 메시지 
+			     String postMsg =  request.getParameter("postMsg");  			//7.배송 메시지 
+			     String payNumber =  request.getParameter("payNumber");  		//8.결제 번호  
+			     String productCode =  request.getParameter("productCode");  	//9.상품코드  
+			     String totalPay =  request.getParameter("totalPay");  			//10.결제금액 
+			     String card =  request.getParameter("card");  					//11.카드정보  
+			     
+			     dto.setMemberId(memberId);
+			     dto.setOrderName(orderName);
+			     dto.setOrderContact(orderContact);
+			     dto.setOrderAddress(orderAddress);
+			     dto.setOrderMsg(orderMsg);
+			     dto.setPostMsg(postMsg);
+			     dto.setPayNumber(payNumber);
+			     dto.setProductCode(productCode);
+			     dto.setTotalPay(totalPay);
+			     dto.setCard(card);
+			     OrderDAO dao = new OrderDAO();
+			     
+			     int inputResult =dao.insert(dto);
+	    		
+	    	}else if(uri.equals("/complete.order")) {
+	    		response.sendRedirect("/order/orderComplete.jsp");
+	    	}else {
+	    		response.sendRedirect("/");
+	    	}
+	    	 
+	   
 	    	
-	    	
-	    }else if(uri.equals( "/")) {
-	    	
-	    }
-	    try {
-	    	
-		 
-		    
+	 
 	    
-
-	     } catch (Exception e) {
-			e.printStackTrace();
-		}
+	  
                                 
 	     
 	   
---
+++ SemiProject/src/main/webapp/order/order.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+ <form action="/order" method="post">
+ 	   <p> 주문정보를 입력해라<p>
+ 	  <input name="memberId" type="text" placeholder="회원아이디" />
+      <input name="orderName" type="text" placeholder="수취인이름" />
+      <input name="orderContect" type="text" placeholder="수취인연락처" />
+      <input name="orderAddress" type="text" placeholder="수취인 주소" />
+      <input name="orderMsg" type="text" placeholder="주문메시지" />
+      <input name="postMsg" type="text" placeholder="배송메시지" />
+      <input name="payNumber" type="text" placeholder="결제번호" />
+      <input name="productCode" type="text" placeholder="상품번호" />
+      <input name="totalPay" type="text" placeholder="결제금액" />
+      <input name="card" type="text" placeholder="카드" />
+      <button type="submit">전송</button>
+    </form>
+
+
+
+</body>
+</html>
